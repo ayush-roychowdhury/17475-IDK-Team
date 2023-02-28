9e146e7915a068c7035c1701254deae22d5ef206
Rick Hanish <rickhanish@utexas.edu>
2023-02-28 12:08:40 -0600

    Began writing methods to interface with hardware database. Will finish these later today.

--- DatabaseSetup.py
+++ DatabaseSetup.py
@@ -43,6 +43,8 @@ def createProject(username, project_id, project_name, project_desc):
     exists = projects.find_one({'project_name': project_name})
     if exists != None:
         return False
+    #Project database has fields for project ID, project name, project description,
+    #and hardware names/quantities currently checked out to that project.
     document = {'project_id': project_id,
                 'project_name': project_name,
                 'project_desc': project_desc,
@@ -50,9 +52,6 @@ def createProject(username, project_id, project_name, project_desc):
     projects.insert_one(document)
     client.close()
     return True
-    #TODO
-    #Project database has fields for project ID, project name, project description,
-    #and hardware names/quantities currently checked out to that project.
 
 # Returns dict of hardware currently checked out if given project exists, False otherwise.
 # Prereq: username is a valid user.
@@ -66,3 +65,55 @@ def selectProject(username, project_name):
         return False
     else:
         return project['hardware']
+
+# TODO: Add similar methods for hardware database.
+# Hardware has name, capacity, availability, and possibly other fields.
+
+# Adds a piece of hardware to the database. Mostly for debugging and administrative
+# uses for now. Note that a negative amt can be used to remove hardware from the database.
+def addHardware(hw_name, amt):
+    from pymongo import MongoClient
+    client = pymongo.MongoClient("mongodb+srv://rickhanish:ECE461LPassword@cluster0.e0ejs3p.mongodb.net/?retryWrites=true&w=majority")
+    db = client.Hardware
+    hw_set = db.HW_Set
+    hw = hw_set.find_one({'name': hw_name})
+    if hw == None:
+        document = {'name': hw_name,
+                    'capacity': amt,
+                    'availability': amt}
+        hw_set.insert_one(document)
+    else:
+        if amt < 0:
+            removal_amt = -amt if -amt <= hw['availability'] else hw['availability']
+            hw['availability'] -= removal_amt
+            hw['capacity'] -= removal_amt
+        else:
+            hw['availability'] += amt
+            hw['capacity'] += amt
+
+# Checks out specified hardware amount from the specified project
+# Returns: 2, if the project doesn't exist with the specified username.
+#          1, if there is not enough capacity to check out all of the
+#             requested hardware. In this case, the remaining capacity
+#             is checked out, which is less than the requested amount.
+#          0, if all of the requested hardware is successfully checked
+#             out.
+def check_out(hw_name, amt, project_name, username):
+    from pymongo import MongoClient
+    client = pymongo.MongoClient("mongodb+srv://rickhanish:ECE461LPassword@cluster0.e0ejs3p.mongodb.net/?retryWrites=true&w=majority")
+    db = client.Projects
+    projects = db[username]
+    project = projects.find_one({'project_name': project_name})
+    if project == None:
+        return 2
+    
+    db = client.Hardware
+    hw_set = db.HW_Set
+    hw = hw_set.find_one({'name': hw_name})
+    if hw['availability'] < amt:
+        #TODO
+        return 1
+    else:
+        #TODO
+        return 0
+    
